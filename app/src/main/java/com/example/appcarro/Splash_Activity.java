259fa60ae3ae3545c6316fdb389fb1019fd99214
lordgrifon <leopr.95@hotmail.com>
2020-05-11 21:37:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appcarro/Splash_Activity.java
@@ -0,0 +1,35 @@
+package com.example.appcarro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abrirJanela();
+            }
+        },5000);
+
+    }
+
+    public void abrirJanela() {
+
+        Intent intent = new Intent(Splash_Activity.this,Login_Activity.class);
+        startActivity(intent);
+
+
+    }
+
+
+}
